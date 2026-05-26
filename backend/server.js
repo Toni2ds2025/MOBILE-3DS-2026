0101feed0ff535b7e280d0880223f1a471e1d3aa
Aka Akasaka <antonio.kuhl.neto@escola.pr.gov.br>
2026-05-26 10:33:09 -0300

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,22 @@
-const { mongoClient } = require('mongodb');
+const { MongoClient } = require('mongodb');
 
-const uri = process.env.MONGO_URI || 'mongodb://localhost:27017/react-native';
+const uri = 'mongodb://127.0.0.1:27017';
+
+const dbName = 'banco1';
 
 const client = new MongoClient(uri);
+
+async function conectar() {
+    try {
+        await client.connect();
+        console.log('✓ Conexão com o MongoDB estabelecida!');
+        //Teste rápido: mostra quantas coleçoes existem no banco de dados
+        const db = client.db();
+        const colecoes = await db.listCollections().toArray();
+        console.log('▤ Coleções encontradas: ', colecoes.length);
+    }catch (err){
+        console.log('✕ Falha ao conectar: ', err.message);
+    }
+}
+
+conectar();
